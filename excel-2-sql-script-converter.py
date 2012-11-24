69f21f57aa9f07ca048c0b1d7fbd5fe9880dabd0
stephen <stephen.j.tong@gmail.com>
2012-11-24 08:18:19 +0800

    结果匹配

--- excel-2-sql-script-converter.py
+++ excel-2-sql-script-converter.py
@@ -2,8 +2,9 @@
 import codecs
 import xlrd
 import sys
+import re
 if len(sys.argv)<2:
-	print "please input xls file name"
+	#print "please input xls file name"
 	exit()
 try:
 	outfile = sys.argv[2]
@@ -11,9 +12,11 @@ except:
 	outfile = 'result.sql'
 wb = xlrd.open_workbook(sys.argv[1])
 f = codecs.open(outfile, 'w', 'utf-8')
-print "set autocommit = 0;"
+#print "set autocommit = 0;"
 f.write("set autocommit = 0;\n" )
-for sheet in wb.sheet_names():
+config_sh = wb.sheet_by_name(u'config')
+sheets = config_sh.col_values(0) #取出第一列
+for sheet in sheets:
 	if not sheet.startswith('tbl_'): #只写tbl_开头的
 		continue
 	sh = wb.sheet_by_name(sheet)
@@ -23,40 +26,42 @@ for sheet in wb.sheet_names():
 	except:
 		pass
 	colums_num = len(colums)
-	print "-- Start processing table "+sheet
-	f.write("-- Start processing table "+sheet+"\n")
-	print "DELETE FROM "+sheet
-	f.write("DELETE FROM "+sheet+";\n")
+	#print "-- Start processing table "+sheet
+	#f.write("-- Start processing table "+sheet+"\n")
+	#print "DELETE FROM "+sheet
+	f.write("delete from `"+sheet+"`;\n")
 	for rownum in range(1, sh.nrows):
 		rowvalues = sh.row_values(rownum)
 		rowvalues = rowvalues[:colums_num]
 		tmp = [i for i in colums if i]; #删除空字段
 		if not tmp: #删除空字段后整个数组为空, 此行为空
 			continue
-		sql = "INSERT INTO "+sheet+" ("
+		sql = "insert into `"+sheet+"` ("
 		for col in colums:
 			sql = sql + "`"+col+"`,"
 		sql = sql[:-1]
-		sql = sql + ") VALUES ("
+		sql = sql + ") values ("
 		tmp = [i for i in rowvalues if i]
 		if not tmp: #全为空
 			continue;
 		for colnum in range(colums_num):
 			if type(rowvalues[colnum]) is str or type(rowvalues[colnum]) is unicode:
 				if not rowvalues[colnum]:
-					sql = sql +"NULL,"
+					sql = sql +"null,"
 				else:
-					sql = sql + "\"" + (rowvalues[colnum]) + "\","
+					sql = sql + "'" + (rowvalues[colnum]) + "',"
 			else:
 				if not rowvalues[colnum]:
-					sql = sql +"0,"
+					sql = sql +"'0',"
 				else:
-					sql = sql + str(int(rowvalues[colnum]))+","
+					tmp_value = str(float(rowvalues[colnum]))
+					tmp_value = re.sub(r'\.0$', '', tmp_value)
+					sql = sql + "'"+ tmp_value +"',"
 		sql = sql[:-1]
 		sql = sql + ");"
-		print sql+""
+		#print sql+""
 		f.write(sql+"\n");
-	print "-- End of processing table "+sheet
-	f.write("-- End of processing table "+sheet+"\n")
-print "commit;"
+	#print "-- End of processing table "+sheet
+	#f.write("-- End of processing table "+sheet+"\n")
+#print "commit;"
 f.write("commit;")
