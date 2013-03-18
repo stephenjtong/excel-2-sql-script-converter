ac0f87c0befefcf189560e12231fcc84e7d1b716
Jishi Tong <jishi.tong@redatoms.com>
2013-03-18 18:14:28 +0800

    打印时间增加星期

--- excel-2-sql-script-converter.py
+++ excel-2-sql-script-converter.py
@@ -15,7 +15,7 @@ except:
 wb = xlrd.open_workbook(sys.argv[1])
 f = codecs.open(outfile, 'w', 'utf-8')
 print "set autocommit = 0;"
-f.write("-- Generated at: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"\n\n");
+f.write("-- Generated at: " + datetime.now().strftime("%Y-%m-%d %A %H:%M:%S")+"\n\n");
 f.write("set autocommit = 0;\n" )
 config_sh = wb.sheet_by_name(u'config')
 sheets = config_sh.col_values(0) #第一列：excel sheet名
